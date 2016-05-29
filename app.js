49f793982007070418db16d75818f5a4121ec73d
boazblake <boazblake@gmail.com>
2016-05-29 09:09:28 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+
+const app = express();
+
+const port = 5000
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+
+app.get('/', (req, resp) => {
+	resp.send('jellow world')
+})
+
+app.get('/books', (req, resp) => {
+	resp.send('hellow books')
+})
+
+
+app.listen(port, function(err){
+	console.log('running server on port: ' + port);
+});
+
+
