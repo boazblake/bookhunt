3ca9c44b3c32d89dda7b88bb3a78dac326794f76
boazblake <boazblake@gmail.com>
2016-05-30 16:05:58 -0500

    works w ejs

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views', './src/views' );
-app.set('view engine', 'jade' );
 
-
+app.set('view engine', 'ejs' );
 
 app.get('/', (req, resp) => {
-    resp.render('index', {list: ['a', 'b']});
+    resp.render('index', {title: 'Coming from List ', list: ['a', 'b']});
 });
 
 app.get('/books', (req, resp) => {
