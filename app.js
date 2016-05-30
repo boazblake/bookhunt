805c51e0d36bb5b861945f9d01bdc2a4e7e375c4
boazblake <boazblake@gmail.com>
2016-05-30 09:24:25 -0500

    added gulp injecting js and scss and auto restart

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
@@ -10,16 +10,14 @@ app.use(express.static('src/views'));
 
 
 app.get('/', (req, resp) => {
-	resp.send('jellow world')
-})
+    resp.send('jellow world');
+});
 
 app.get('/books', (req, resp) => {
-	resp.send('hellow books')
-})
-
-
-app.listen(port, function(err){
-	console.log('running server on port: ' + port);
+    resp.send('hellow books');
 });
 
 
+app.listen(port, function(err) {
+    console.log('running server on port: ' + port);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,54 @@
 const gulp = require('gulp');
+const jscs = require('gulp-jshint');
 const jshint = require('gulp-jshint');
+const nodemon = require('gulp-nodemon');
 
-const jsFiles = ['8.js', 'src/**/*.js'];
+const jsFiles = ['*.js', 'src/**/*.js'];
 
-gulp.task('style', ()=>{
-	gulp.src(jsFiles)
-		.pipe(jshint())
+gulp.task('style', () => {
+    return gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
 });
+
+gulp.task('inject', () =>{
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css', 
+                                './public/js/*.js'], {read:false});
+
+    var injectOptions = {
+      ignorePath: '/public'
+    };
+
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    }
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], () => {
+  var options = {
+    script: 'app.js',
+    delayTime: 1,
+    env: {
+      'PORT': 3000
+    },
+    watch: jsFiles
+  }
+
+  return nodemon(options)
+    .on('restart', function(ev) {
+      console.log('Restarting App...');
+    })
+} )
