fda54bd8f8139926ed2c37233b774ef86e7b20ec
boazblake <boazblake@gmail.com>
2016-05-31 18:21:11 -0500

    connecting nd pulling to and from good reads

--- src/services/goodReadsService.js
+++ src/services/goodReadsService.js
@@ -1,10 +1,35 @@
+
+const http = require('http');
+const xml2js = require('xml2js');
+const parser = xml2js.Parser({explicitArray: false});
+
+
 const goodReadsService = () => {
 
 	var getBookById = (id, cb) => {
-		cb(null, {description: 'our description'});
+
+		var options = {
+			host: 'www.goodreads.com',
+			path: '/book/show/656?format=xml&key=276hVIls8qy3OV8qivy2g'
+		};
+		var callBackFunction = (response) => {
+			var str = '';
+
+			response.on('data', (chunk) => {
+				str += chunk;
+			});
+			response.on('end', () => {
+				console.log('str>>>>', str);
+				parser.parseString(str,
+					(err, result) => {
+						cb(null, result.GoodreadsResponse.book);
+				});
+			});
+		};
+		
+		http.request(options, callBackFunction).end();
 
 	};
-
 	return {
 		getBookById: getBookById
 	};
